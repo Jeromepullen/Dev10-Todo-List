f44418cba08f4d6f5ee488f9a7a8fbfc54be450d
Jerome Pullen Jr <jerome.pullen@gmail.com>
2019-10-28 00:33:18 -0500

    Add files via upload

---
+++ mainScript.js
@@ -0,0 +1,51 @@
+/*
+Name: Jerome Pullen Jr.
+*/
+
+
+function setMinDate() {
+	var date = new Date();
+	var y = date.getFullYear();
+	if (date.getMonth() < 9) {
+		m = "0" + (date.getMonth() + 1);
+	} else { m = date.getMonth() + 1;};
+	if (date.getDate() < 10) {
+		d = "0" + date.getDate();
+	} else { d = date.getDate();};
+	var today = y + "-" + m + "-" + d;
+	document.getElementById("dayDue").min = today;
+}
+
+function createTask() {
+	var task = document.getElementById("taskName").value;
+	var dueDate = new Date(document.getElementById("dayDue").value);
+	var currentDate = new Date();
+	var milliDue = Math.abs(currentDate - dueDate);
+	var daysDue = Math.ceil(milliDue/(1000 * 60 * 60 * 24));
+	var string = '<span>' + task + '</span>' +
+		'<button type="button" class="close" data-dismiss="alert" aria-label="Close" style="padding: 5px 10px 5px 10px;">' +
+			'<span aria-hidden="true">&times;</span>' +
+		'</button>' +
+		'<br>' + daysDue + " day(s)";
+	var node = document.createElement("div");
+	styleTask(node, daysDue);
+	node.innerHTML = string;
+	document.getElementById("toDoList").appendChild(node);
+}
+
+function styleTask(elmnt, day) {
+	elmnt.setAttribute("class", "col-xs-auto alert alert-warning alert-dismissible show");
+	elmnt.setAttribute("role", "alert");
+	elmnt.style.padding = "20px 35px 20px 20px";
+	elmnt.style.borderRadius = "15px 15px 15px 15px";
+	if (day >= 7) {
+		elmnt.style.backgroundColor = "rgba(128, 128, 128, 0.3)";
+	}
+	if (day < 7 && day >= 3) {
+		elmnt.style.backgroundColor = "rgba(255, 255, 0, 0.3)";
+	}
+	if (day < 3 && day >= 1) {
+		elmnt.style.backgroundColor = "rgba(255, 0, 0, 0.3)";
+	}	
+	return elmnt;
+}
